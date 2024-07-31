ac33a9186be473c2456497ea3a26a8f33c26817f
Okan Emre KOCA <oknakoca@gmail.com>
2024-07-31 14:41:08 +0200

    first bit of norme

--- src/ctx.c
+++ src/ctx.c
@@ -6,13 +6,13 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 11:32:40 by okoca             #+#    #+#             */
-/*   Updated: 2024/07/31 13:40:49 by okoca            ###   ########.fr       */
+/*   Updated: 2024/07/31 14:40:52 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-t_ctx	cb_init_ctx()
+t_ctx	cb_init_ctx(void)
 {
 	t_ctx	ctx;
 
@@ -34,7 +34,7 @@ int	cb_init_image(t_ctx *ctx)
 	if (!ctx->img.img)
 		return (1);
 	ctx->img.buffer = mlx_get_data_addr(ctx->img.img, &ctx->img.bits_per_pixel,
-		&ctx->img.line_size, &ctx->img.endian);
+			&ctx->img.line_size, &ctx->img.endian);
 	return (0);
 }
 
@@ -43,7 +43,8 @@ int	cb_init_mtx(t_ctx *ctx)
 	ctx->mlx = mlx_init();
 	if (!ctx->mlx)
 		return (1);
-	ctx->window = mlx_new_window(ctx->mlx, SCREEN_WIDTH, SCREEN_HEIGHT, PROG_NAME);
+	ctx->window = mlx_new_window(ctx->mlx, SCREEN_WIDTH,
+			SCREEN_HEIGHT, PROG_NAME);
 	if (!ctx->mlx)
 	{
 		cb_free_all(ctx);
@@ -61,9 +62,6 @@ int	cb_init_mtx(t_ctx *ctx)
 	}
 	ctx->map = init_map();
 	if (!ctx->map)
-	{
-		cb_free_all(ctx);
-		return (1);
-	}
+		return (cb_free_all(ctx), 1);
 	return (0);
 }
